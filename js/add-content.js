35b00661daf4d727f0fd9b74a4546f940a3d7814
racarter1215 <racarter1215@gmail.com>
2020-02-05 16:06:33 -0800

    lots of adds to site via JS for study

--- js/add-content.js
+++ js/add-content.js
@@ -1,1 +1,13 @@
-alert("Do you have enough money for these cars?");
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good Evening!';
+} else if (hourNow > 12) {
+    greeting = 'UR BAD AT STUFF';
+} else {
+    greeting = 'Welcome!' ;
+}
+
+document.write('<h1>' + greeting + '</h1>');
