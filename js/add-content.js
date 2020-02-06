4024dfc9781097dcb07f842057d1f63f2af9ad42
racarter1215 <racarter1215@gmail.com>
2020-02-05 16:28:06 -0800

    Finished website with JS for now

--- js/add-content.js
+++ js/add-content.js
@@ -7,8 +7,18 @@ if (hourNow > 18) {
 } else if (hourNow > 12) {
     greeting = 'UR BAD AT STUFF';
 } else {
-    greeting = 'Welcome!' ;
+    greeting = 'Welcome!';
 }
 
 document.write('<h1>' + greeting + '</h1>');
 
+var greeting = prompt('What is best in life? (1 = Wrong Answer 2 = Right Answer)');
+var reply;
+
+if (greeting = 1) {
+    reply = 'Coding';
+} else {
+    reply = 'Crush your enemies, drive them before you, and hear the lamentations of their women!';
+}
+
+document.write('<body>' + reply + '</body>');
