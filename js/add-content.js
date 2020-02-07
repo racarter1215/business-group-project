c0b6ece78558a049b87dc0f45d37058cce1dfd32
racarter1215 <racarter1215@gmail.com>
2020-02-06 20:45:28 -0800

    added the ask about cars and tried to add a while loop

--- js/add-content.js
+++ js/add-content.js
@@ -1,24 +1,56 @@
-var today = new Date();
-var hourNow = 23;
-var greeting;
+ 
+    var today = new Date();
+    var hourNow = 23;
+    var greeting;
+    
 
-if (hourNow > 18) {
-    greeting = 'Good Evening!';
-} else if (hourNow > 12) {
-    greeting = 'UR BAD AT STUFF';
-} else {
-    greeting = 'Welcome!';
+    if (hourNow > 18) {
+        greeting = 'Good Evening!';
+    } else if (hourNow > 12) {
+        greeting = 'Good Afternoon';
+    } else {
+        greeting = 'Welcome!';
+     }
+
+    document.write(greeting);
+
+    
+
+
+function askConan() {
+    var question = prompt('What is best in life? 1 = What would a programmer say? 2 = What would Conan say?');
+    console.log(typeof(question))
+    var text;
+    
+    if( question === '1') {
+        text = 'Coding!';
+    } else if (question === '2') {
+        text = 'Crush your enemies, drive them before you, and hear the lamentations of their women';
+    } else {
+        text = 'Ur bad at stuff';
+    }
+    
+    return text;
 }
 
-document.write('<h1>' + greeting + '</h1>');
-
-var question = prompt('What is best in life? (1 = What would a programmer say?, 2 = What would Conan say?)');
-var reply;
-
-if (question = 1) {
-    reply = 'Coding';
-} else if (question = 2) {
-    reply = 'Crush your enemies, drive them before you, and hear the lamentations of their women!';
+function askQuantity() {
+    var question = prompt('How many cars would you like?');
+    console.log(typeof(question))
+    var text;
+    
+    if( question >= '1') {
+        text = 'Excellent, let me draw up the paperwork';
+    } else if (question >= '5') {
+        text = 'Big spender! Would you like some champagne?';
+    } else if (question = '0') {
+        text = 'No credit no car!';
+    }
+    
+    return text;
 }
 
-document.write('<h1>' + reply + '</h1>');
+var question; 
+    while (question > '7') {
+    answer = prompt('Please do not waste my time. Give a real number');
+    }
+    
